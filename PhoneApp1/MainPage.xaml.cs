d0390ed10aa38e9340404ea4fb1c96f2c2ebcc6d
Denscorov <wally.eva@yandex.ru>
2016-01-05 11:50:50 +0200

    qqq

---
+++ PhoneApp1/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using PhoneApp1.Resources;
+
+namespace PhoneApp1
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Конструктор
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        
+    }
+}
