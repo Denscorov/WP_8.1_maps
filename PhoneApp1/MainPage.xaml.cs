4d4465aa9f730d75b00d6fb793bf8b5f00401eeb
Denscorov <wally.eva@yandex.ru>
2016-01-06 17:14:07 +0200

    333

--- PhoneApp1/MainPage.xaml.cs
+++ PhoneApp1/MainPage.xaml.cs
@@ -9,8 +9,10 @@ using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using PhoneApp1.Resources;
 using Microsoft.Phone.Maps.Toolkit;
+using Microsoft.Phone.Maps.Controls;
 using System.Device.Location;
-
+using System.Threading.Tasks;
+using Microsoft.Phone.Maps.Toolkit;
 
 namespace PhoneApp1
 {
@@ -22,6 +24,7 @@ namespace PhoneApp1
             InitializeComponent();
             
         }
+        MapLayer ml = new MapLayer();
 
         private void Location_Click(object sender, EventArgs e)
         {
@@ -30,16 +33,32 @@ namespace PhoneApp1
             geowach.PositionChanged += Geowach_PositionChanged;
             geowach.StatusChanged += Geowach_StatusChanged;
             geowach.Start();
+            
         }
 
         private void Geowach_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
         {
-            map.Center = e.Position.Location;
+            //map.Center = e.Position.Location;
+            Pushpin p = new Pushpin();
+            p.Content = e.Position.Location.Course.ToString();
+            p.GeoCoordinate = e.Position.Location;
+
+            MapChildCollection mc = new MapChildCollection();
+            mc.Add(p);
+            MapOverlay mo = new MapOverlay() { Content = p };
+            ml.Add(mo);
+
+            
         }
 
         private void Geowach_StatusChanged(object sender, GeoPositionStatusChangedEventArgs e)
         {
-            SystemTray.ProgressIndicator = new ProgressIndicator() { Text = e.Status.ToString(),IsVisible = true };
+           
+            SystemTray.ProgressIndicator = new ProgressIndicator();
+            SystemTray.ProgressIndicator.Text = e.Status.ToString();
+            SystemTray.ProgressIndicator.IsVisible = true;
+            Task.Delay(1000);
+            SystemTray.IsVisible = false;
         }
     }
 }
