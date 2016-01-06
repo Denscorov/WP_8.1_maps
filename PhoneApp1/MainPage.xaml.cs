c7cad430704680800d3c6152a6c912b8287aeb24
Denscorov <wally.eva@yandex.ru>
2016-01-06 21:36:02 +0200

    xaml

--- PhoneApp1/MainPage.xaml.cs
+++ PhoneApp1/MainPage.xaml.cs
@@ -6,6 +6,9 @@ using System.Windows.Shapes;
 using System.Windows.Media;
 using Microsoft.Phone.Maps.Controls;
 using Microsoft.Phone.Controls;
+using Microsoft.Phone.Maps.Services;
+using System.Collections.Generic;
+using Windows.Devices.Geolocation;
 
 namespace PhoneApp1
 {
@@ -15,9 +18,31 @@ namespace PhoneApp1
         public MainPage()
         {
             InitializeComponent();
+            this.GetCoordinates();
             
         }
-       
+
+        private async void GetCoordinates()
+        {
+            // Get the phone's current location.
+            Geolocator MyGeolocator = new Geolocator();
+            MyGeolocator.DesiredAccuracyInMeters = 1;
+            Geoposition MyGeoPosition = null;
+            try
+            {
+                MyGeoPosition = await MyGeolocator.GetGeopositionAsync(TimeSpan.FromMinutes(1), TimeSpan.FromSeconds(10));
+                MyCoordinates.Add(new GeoCoordinate(MyGeoPosition.Coordinate.Point.Position.Latitude, MyGeoPosition.Coordinate.Point.Position.Longitude));
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Location is disabled in phone settings or capabilities are not checked.");
+            }
+            catch (Exception ex)
+            {
+                // Something else happened while acquiring the location.
+                MessageBox.Show(ex.Message);
+            }
+        }
 
         private void Location_Click(object sender, EventArgs e)
         {
@@ -57,5 +82,43 @@ namespace PhoneApp1
             MessageBox.Show(e.Status.ToString());
         }
 
+
+        RouteQuery MyQuery = null;
+        GeocodeQuery Mygeocodequery = null;
+        List<GeoCoordinate> MyCoordinates = new List<GeoCoordinate>();
+
+        private void route_Click(object sender, EventArgs e)
+        {
+            Mygeocodequery = new GeocodeQuery();
+            Mygeocodequery.SearchTerm = "хмельницький зарічанська 10";
+            Mygeocodequery.GeoCoordinate = new GeoCoordinate(46.202215,25.020216);
+            Mygeocodequery.QueryCompleted += Mygeocodequery_QueryCompleted;
+            Mygeocodequery.QueryAsync();
+
+        }
+
+        private void Mygeocodequery_QueryCompleted(object sender, QueryCompletedEventArgs<System.Collections.Generic.IList<MapLocation>> e)
+        {
+            if (e.Error == null)
+            {
+                MyQuery = new RouteQuery();
+                MyCoordinates.Add(e.Result[0].GeoCoordinate);
+                MyQuery.Waypoints = MyCoordinates;
+                MyQuery.QueryCompleted += MyQuery_QueryCompleted;
+                MyQuery.QueryAsync();
+                Mygeocodequery.Dispose();
+            }
+        }
+
+        private void MyQuery_QueryCompleted(object sender, QueryCompletedEventArgs<Route> e)
+        {
+            if (e.Error == null)
+            {
+                Route MyRoute = e.Result;
+                MapRoute MyMapRoute = new MapRoute(MyRoute);
+                map.AddRoute(MyMapRoute);
+                MyQuery.Dispose();
+            }
+        }
     }
 }
